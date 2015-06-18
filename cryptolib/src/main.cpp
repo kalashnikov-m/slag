963606630f1faeedd79a36d7c040a91218c275ed
mkalashnikov <M.Kalashnikov@aladdin-rd.ru>
2015-06-18 19:06:35 +0300

    Google Test!

--- cryptolib/src/main.cpp
+++ cryptolib/src/main.cpp
@@ -12,14 +12,71 @@
 
 #include "huge.h"
 
+#include "gtest/gtest.h"
+
 using namespace std;
 
 /*
  * 
  */
-int main(int argc, char** argv)
+
+static bool ASSERT_BYTES_EQ(byte* f1, byte* l1, byte* f2, byte* l2)
+{
+    bool ret = false;
+    
+    while((f1 != l1) && (*f1 == 0x00))
+	++f1;
+	
+    //printf("%02x\n", *f2);
+	
+    while((f2 != l2) && (*f2 == 0x00))
+	++f2;
+    
+    //printf("%02x\n", *f2);
+    //printf("%02x, %02x\n", *f1, *f2);
+    
+    /*while(f1 != l1 && f2 != l2)
+    {
+	if(*f1 != *f2)
+	    break;
+	++f1;
+	++f2;
+    }*/
+	
+    //printf("%02x, %02x\n", *f1, *f2);
+	
+    //ret = ((f1 == l1) && (f2 == l2));
+	
+    //printf("ret: %d", ret);
+	
+    return ret;
+}
+ 
+TEST(HUGE_TEST, uadd_test)
 {
     byte a[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1a, 0x03};
+    byte b[] = {0x00, 0x00, 0x00, 0x11};
+    byte actual[8] = {0x00};
+    byte expected[] = {0x1a, 0x14};
+    
+    uadd(end(actual), begin(a), end(a), begin(b), end(b));
+    
+    for_each(begin(expected), end(expected), [](byte b)
+    {
+        printf("%02x.", b);
+    });
+    
+    //bool eq = ASSERT_BYTES_EQ(/*std::begin(expected), std::end(expected)*/expected, expected + 2, std::begin(actual), std::end(actual));
+    //EXPECT_TRUE(eq);
+}
+ 
+int main(int argc, char** argv)
+{
+    ::testing::InitGoogleTest(&argc, argv);
+    
+    return RUN_ALL_TESTS();
+
+    /*byte a[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x1a, 0x03};
     byte b[] = {0x00, 0x00, 0x00, 0x11};
     byte c[8] = {0x00};
     byte d[8] = {0x00};
@@ -35,8 +92,8 @@ int main(int argc, char** argv)
         printf("%02x.", b);
     });
     
-    printf("\n");*/
-    }
+    printf("\n");
+    }*/
 
     /*{ // ===== UMUL =====
 	byte a[] = {0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x10};
