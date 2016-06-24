8d6bc650b4bfe91ee53b162fb767e4724f239cf4
kalashnikov maxim <maxim_1981@inbox.ru>
2016-06-24 05:36:16 +0300

    некоторые алгоритмы перенесены в отдельный namespace

---
+++ cryptolib/src/algorithm.hpp
@@ -0,0 +1,65 @@
+
+#ifndef ALGORITHM_HPP
+#define ALGORITHM_HPP
+
+namespace cry {
+
+template <class T>
+T Gcd(const T& lhs, const T& rhs) 
+{
+  auto pair = std::minmax(lhs, rhs);
+
+    auto r1(pair.second);
+    auto r2(pair.first);
+
+    while (r2 != 0)
+    {
+        auto rem = r1 % r2;
+        r1             = r2;
+        r2             = rem;
+    }
+
+    return r1;
+}
+
+template <class T>
+bool ModInverse(T& inverse, const T& a, const T& modulus) 
+{
+  if (a >= modulus)
+        return false;
+
+    T r1(modulus);
+    T r2(a);
+
+    T t1 = 0;
+    T t2 = 1;
+
+    while (r2 > 0)
+    {
+        T q;
+        T r;
+
+        r1.DivRem(q, r, r2);
+        r1 = r2;
+        r2 = r;
+
+        T t = (t1 - q * t2);
+        t1        = t2;
+        t2        = t;
+    }
+
+    if (r1 != 1)
+        return false;
+
+    if (t1 < 0)
+    {
+        t1 += modulus;
+    }
+
+    inverse = t1;
+
+    return true;
+}
+}
+
+#endif
