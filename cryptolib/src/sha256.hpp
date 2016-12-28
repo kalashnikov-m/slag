16cf5b9a5d37cabc291be635e11638c1e3e3ceed
kalashnikov-m <maxim_1981@inbox.ru>
2016-12-28 15:58:37 +0300

    sha256

---
+++ cryptolib/src/sha256.hpp
@@ -0,0 +1,205 @@
+#ifndef SHA256_HPP
+#define SHA256_HPP
+
+namespace cry {
+
+    class SHA256 {
+
+      public:
+        SHA256() {}
+        ~SHA256() {}
+
+        void Init() {
+            m_Idx = 0;
+            m_Len = 0;
+
+            m_Digest[0] = 0x6a09e667;
+            m_Digest[1] = 0xbb67ae85;
+            m_Digest[2] = 0x3c6ef372;
+            m_Digest[3] = 0xa54ff53a;
+            m_Digest[4] = 0x510e527f;
+            m_Digest[5] = 0x9b05688c;
+            m_Digest[6] = 0x1f83d9ab;
+            m_Digest[7] = 0x5be0cd19;
+        }
+
+        void Update(const std::vector<uint8_t>& m) {
+            std::vector<uint8_t>::const_iterator it(m.begin()), end(m.end());
+
+            while (it != end) {
+                m_Block[m_Idx++] = *it++;
+                m_Len += 8;
+
+                if (m_Idx == 0x40) // 64
+                {
+                    transform();
+                }
+            }
+        }
+
+        void Final(std::vector<uint8_t>& digest) {
+            if (m_Idx > 55) {
+                m_Block[m_Idx++] = 0x80;
+                while (m_Idx < 64) {
+                    m_Block[m_Idx++] = 0x00;
+                }
+
+                transform();
+
+                while (m_Idx < 56) {
+                    m_Block[m_Idx++] = 0x00;
+                }
+            } else {
+                m_Block[m_Idx++] = 0x80;
+
+                while (m_Idx < 56) {
+                    m_Block[m_Idx++] = 0x00;
+                }
+            }
+
+            m_Block[m_Idx++] = (m_Len & 0xFFFFFFFF00000000) >> 24;
+            m_Block[m_Idx++] = (m_Len & 0xFFFFFFFF00000000) >> 16;
+            m_Block[m_Idx++] = (m_Len & 0xFFFFFFFF00000000) >> 8;
+            m_Block[m_Idx++] = (m_Len & 0xFFFFFFFF00000000);
+
+            m_Block[m_Idx++] = (m_Len & 0x00000000FFFFFFFF) >> 24;
+            m_Block[m_Idx++] = (m_Len & 0x00000000FFFFFFFF) >> 16;
+            m_Block[m_Idx++] = (m_Len & 0x00000000FFFFFFFF) >> 8;
+            m_Block[m_Idx++] = (m_Len & 0x00000000FFFFFFFF);
+
+            transform();
+
+            digest.resize(32);
+
+            digest[0] = (m_Digest[0] >> 24) & 0x000000ff;
+            digest[1] = (m_Digest[0] >> 16) & 0x000000ff;
+            digest[2] = (m_Digest[0] >> 8) & 0x000000ff;
+            digest[3] = (m_Digest[0] >> 0) & 0x000000ff;
+
+            digest[4] = (m_Digest[1] >> 24) & 0x000000ff;
+            digest[5] = (m_Digest[1] >> 16) & 0x000000ff;
+            digest[6] = (m_Digest[1] >> 8) & 0x000000ff;
+            digest[7] = (m_Digest[1] >> 0) & 0x000000ff;
+
+            digest[8] = (m_Digest[2] >> 24) & 0x000000ff;
+            digest[9] = (m_Digest[2] >> 16) & 0x000000ff;
+            digest[10] = (m_Digest[2] >> 8) & 0x000000ff;
+            digest[11] = (m_Digest[2] >> 0) & 0x000000ff;
+
+            digest[12] = (m_Digest[3] >> 24) & 0x000000ff;
+            digest[13] = (m_Digest[3] >> 16) & 0x000000ff;
+            digest[14] = (m_Digest[3] >> 8) & 0x000000ff;
+            digest[15] = (m_Digest[3] >> 0) & 0x000000ff;
+
+            digest[16] = (m_Digest[4] >> 24) & 0x000000ff;
+            digest[17] = (m_Digest[4] >> 16) & 0x000000ff;
+            digest[18] = (m_Digest[4] >> 8) & 0x000000ff;
+            digest[19] = (m_Digest[4] >> 0) & 0x000000ff;
+
+            digest[20] = (m_Digest[5] >> 24) & 0x000000ff;
+            digest[21] = (m_Digest[5] >> 16) & 0x000000ff;
+            digest[22] = (m_Digest[5] >> 8) & 0x000000ff;
+            digest[23] = (m_Digest[5] >> 0) & 0x000000ff;
+
+            digest[24] = (m_Digest[6] >> 24) & 0x000000ff;
+            digest[25] = (m_Digest[6] >> 16) & 0x000000ff;
+            digest[26] = (m_Digest[6] >> 8) & 0x000000ff;
+            digest[27] = (m_Digest[6] >> 0) & 0x000000ff;
+
+            digest[28] = (m_Digest[7] >> 24) & 0x000000ff;
+            digest[29] = (m_Digest[7] >> 16) & 0x000000ff;
+            digest[20] = (m_Digest[7] >> 8) & 0x000000ff;
+            digest[31] = (m_Digest[7] >> 0) & 0x000000ff;
+        }
+
+      protected:
+        uint32_t inline ROTR(uint32_t x, int shift) {
+            x = ((x >> shift) | (x << (32 - shift)));
+
+            return x;
+        }
+
+        uint32_t inline SUM0(uint32_t x) { return (ROTR(x, 2) ^ ROTR(x, 13) ^ ROTR(x, 22)); }
+
+        uint32_t inline SUM1(uint32_t x) { return (ROTR(x, 6) ^ ROTR(x, 11) ^ ROTR(x, 25)); }
+
+        uint32_t inline sigma_0(uint32_t x) { return (ROTR(x, 7) ^ ROTR(x, 18) ^ (x >> 3)); }
+
+        uint32_t inline sigma_1(uint32_t x) { return (ROTR(x, 17) ^ ROTR(x, 19) ^ (x >> 10)); }
+
+        uint32_t inline Ch(uint32_t x, uint32_t y, uint32_t z) { return ((x & y) ^ (~(x) & (z))); }
+
+        uint32_t inline Maj(uint32_t x, uint32_t y, uint32_t z) { return ((x & y) ^ (x & z) ^ (y & z)); }
+
+        void transform() {
+            uint32_t W[64] = {0x00};
+
+            uint32_t a, b, c, d, e, f, g, h;
+
+            // digest init
+            a = m_Digest[0];
+            b = m_Digest[1];
+            c = m_Digest[2];
+            d = m_Digest[3];
+            e = m_Digest[4];
+            f = m_Digest[5];
+            g = m_Digest[6];
+            h = m_Digest[7];
+
+            for (int t = 0; t < 16; ++t) {
+                W[t] = m_Block[t * 4] << 24;
+                W[t] |= m_Block[t * 4 + 1] << 16;
+                W[t] |= m_Block[t * 4 + 2] << 8;
+                W[t] |= m_Block[t * 4 + 3];
+            }
+
+            for (int t = 16; t < 64; ++t) {
+                W[t] = sigma_1(W[t - 2]) + W[t - 7] + sigma_0(W[t - 15]) + W[t - 16];
+            }
+
+            // main cycle
+            uint32_t T1 = 0;
+            uint32_t T2 = 0;
+
+            for (int t = 0; t < 64; ++t) {
+
+                T1 = h + SUM1(e) + Ch(e, f, g) + K[t] + W[t];
+                T2 = SUM0(a) + Maj(a, b, c);
+
+                h = g;
+                g = f;
+                f = e;
+                e = d + T1;
+                d = c;
+                c = b;
+                b = a;
+                a = T1 + T2;
+            }
+
+            m_Digest[0] += a;
+            m_Digest[1] += b;
+            m_Digest[2] += c;
+            m_Digest[3] += d;
+            m_Digest[4] += e;
+            m_Digest[5] += f;
+            m_Digest[6] += g;
+            m_Digest[7] += h;
+
+            m_Idx = 0;
+        }
+
+      private:
+        uint32_t m_Digest[8]; // 256
+        uint8_t m_Block[64];  // 512
+        uint8_t m_Idx;
+        uint64_t m_Len;
+
+        const uint32_t K[64] = {
+            0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5, 0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174, 0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa,
+            0x5cb0a9dc, 0x76f988da, 0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967, 0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85, 0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
+            0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070, 0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3, 0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
+        };
+    };
+}
+
+#endif
