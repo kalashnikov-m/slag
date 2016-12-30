49372df227e90f04139adc538c01e80b22f06e5c
kalashnikov-m <maxim_1981@inbox.ru>
2016-12-30 12:52:57 +0300

    OID

--- cryptolib/src/Pkcs1v1_5.hpp
+++ cryptolib/src/Pkcs1v1_5.hpp
@@ -4,89 +4,92 @@
 #include <random>
 
 // struct OID
+namespace cry {
 
-class PKCS1_v1_5 {
+    class EME_PKCS1_v1_5 {
 
-  public:
-    std::vector<uint8_t> Encode(const std::vector<uint8_t>& message, size_t k) {
-        size_t mLen = message.size();
-        size_t psLen = k - mLen - 3;
+      public:
+        std::vector<uint8_t> Encode(const std::vector<uint8_t>& message, size_t k) {
+            size_t mLen = message.size();
+            size_t psLen = k - mLen - 3;
 
-        std::vector<uint8_t> ps(psLen);
+            std::vector<uint8_t> ps(psLen);
 
-        std::random_device rd;
-        std::mt19937 gen(rd());
-        std::uniform_int_distribution<> uid(1, 255);
+            std::random_device rd;
+            std::mt19937 gen(rd());
+            std::uniform_int_distribution<> uid(1, 255);
 
-        std::generate(std::begin(ps), std::end(ps), [&uid, &gen]() { return uid(gen); });
+            std::generate(std::begin(ps), std::end(ps), [&uid, &gen]() { return uid(gen); });
 
-        std::vector<uint8_t> em(k);
-        std::vector<uint8_t>::iterator it = em.begin(), end(em.end());
+            std::vector<uint8_t> em(k);
+            std::vector<uint8_t>::iterator it = em.begin(), end(em.end());
 
-        *it++ = 0x00;
-        *it++ = 0x02;
+            *it++ = 0x00;
+            *it++ = 0x02;
 
-        std::copy(ps.begin(), ps.end(), it);
+            std::copy(ps.begin(), ps.end(), it);
 
-        it += psLen;
+            it += psLen;
 
-        *it++ = 0x00;
+            *it++ = 0x00;
 
-        std::copy(message.begin(), message.end(), it);
+            std::copy(message.begin(), message.end(), it);
 
-        return em;
-    }
+            return em;
+        }
 
-    std::vector<uint8_t> Decode(const std::vector<uint8_t>& message, size_t k) { return std::vector<uint8_t>(); }
+        std::vector<uint8_t> Decode(const std::vector<uint8_t>& message, size_t k) { return std::vector<uint8_t>(); }
 
-  private:
-};
+      private:
+    };
 
-class EMSA_PKCS1_v1_5 {
+    class EMSA_PKCS1_v1_5 {
 
-  public:
-    template <class DigestType>
-    std::vector<uint8_t> Encode(const std::vector<uint8_t>& message, size_t emLen) {
+      public:
+        template <class DigestType>
+        std::vector<uint8_t> Encode(const std::vector<uint8_t>& message, size_t emLen) {
 
-        std::vector<uint8_t> hash;
+            std::vector<uint8_t> hash;
 
-        DigestType digest;
-        digest.Init();
-        digest.Update(message);
-        digest.Final(hash);
+            DigestType digest;
+            digest.Init();
+            digest.Update(message);
+            digest.Final(hash);
 
-        std::vector<uint8_t> oid = {0x30, 0x21, 0x30, 0x09, 0x06, 0x05, 0x2b, 0x0e, 0x03, 0x02, 0x1a, 0x05, 0x00, 0x04, 0x14};
-        size_t oidLen = oid.size();
-        size_t hLen = hash.size();
-        size_t tLen = hLen + oidLen;
+            // std::vector<uint8_t> oid = {0x30, 0x21, 0x30, 0x09, 0x06, 0x05, 0x2b, 0x0e, 0x03, 0x02, 0x1a, 0x05, 0x00, 0x04, 0x14};
+            auto oid = OID<DigestType>::value();
+            size_t oidLen = oid.size();
+            size_t hLen = hash.size();
+            size_t tLen = hLen + oidLen;
 
-        size_t psLen = emLen - tLen - 3;
+            size_t psLen = emLen - tLen - 3;
 
-        std::vector<uint8_t> PS(psLen, 0xFF);
+            std::vector<uint8_t> PS(psLen, 0xFF);
 
-        std::vector<uint8_t> EM(emLen);
+            std::vector<uint8_t> EM(emLen);
 
-        std::vector<uint8_t>::iterator it(EM.begin()), end(EM.end());
+            std::vector<uint8_t>::iterator it(EM.begin()), end(EM.end());
 
-        *it++ = 0x00;
-        *it++ = 0x01;
+            *it++ = 0x00;
+            *it++ = 0x01;
 
-        std::copy(PS.begin(), PS.end(), it);
-        it += psLen;
+            std::copy(PS.begin(), PS.end(), it);
+            it += psLen;
 
-        *it++ = 0x00;
+            *it++ = 0x00;
 
-        std::copy(oid.begin(), oid.end(), it);
-        it += oidLen;
+            std::copy(oid.begin(), oid.end(), it);
+            it += oidLen;
 
-        std::copy(hash.begin(), hash.end(), it);
+            std::copy(hash.begin(), hash.end(), it);
 
-        return EM;
-    }
+            return EM;
+        }
 
-    std::vector<uint8_t> Decode(const std::vector<uint8_t>& message, size_t k) { return std::vector<uint8_t>(); }
+        std::vector<uint8_t> Decode(const std::vector<uint8_t>& message, size_t k) { return std::vector<uint8_t>(); }
 
-  private:
-};
+      private:
+    };
+}
 
 #endif
