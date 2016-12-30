abec0291fae2696df974bac32066a4956629963f
kalashnikov-m <maxim_1981@inbox.ru>
2016-12-30 12:56:21 +0300

    fix

--- cryptolib/src/main.cpp
+++ cryptolib/src/main.cpp
@@ -9,61 +9,39 @@
 
 #include <algorithm.hpp>
 #include <sha1.hpp>
+#include <oid.hpp>
 #include <sha512.hpp>
+#include <sha384.hpp>
 #include <sha224.hpp>
 #include <sha256.hpp>
+#include <pkcs1v1_5.hpp>
 
 #include <HugeTest.cpp>
 #include <HugeCoreTest.cpp>
 
 #include <vector>
+#include <random>
 
 using namespace std;
 
 int main(int argc, char** argv) {
-    const uint8_t m1[] = {"abc"};
-    const uint8_t m2[] = {""};
-    const uint8_t m3[] = {"abcdbcdecdefdefgefghfghighijhijkijkljklmklmnlmnomnopnopq"};
-    const uint8_t m4[] = {"abcdefghbcdefghicdefghijdefghijkefghijklfghijklmghijklmnhijklmnoijklmnopjklmnopqklmnopqrlmnopqrsmnopqrstnopqrstu"};
 
-    std::vector<uint8_t> digest;
+    EME_PKCS1_v1_5 eme;
+    EMSA_PKCS1_v1_5 emsa;
+    
+    std::vector<uint8_t> message{'*','*','*','*','*'};
 
-    SHA512 hash;
-
-    // hash.Init();
-    // hash.Update(std::vector<uint8_t>(1000000, 'a'));
-    // hash.Final(digest);
-
-    hash.Init();
-    hash.Update(std::vector<uint8_t>(m1, m1 + sizeof(m1) / sizeof(uint8_t) - 1));
-    hash.Final(digest);
-
-    std::for_each(digest.begin(), digest.end(), [](uint8_t b) { printf("%02x", b); });
-    std::cout << "\n";
-
-    /*hash.Init();
-    hash.Update(std::vector<uint8_t>(m2, m2 + sizeof(m2) / sizeof(uint8_t) - 1));
-    hash.Final(digest);
-
-    //std::for_each(digest.begin(), digest.end(), [](uint8_t b) { printf("%02x", b); });
-    //std::cout << "\n";
-
-    hash.Init();
-    hash.Update(std::vector<uint8_t>(m3, m3 + sizeof(m3) / sizeof(uint8_t) - 1));
-    hash.Final(digest);
-
-    //std::for_each(digest.begin(), digest.end(), [](uint8_t b) { printf("%02x", b); });
-    //std::cout << "\n";
-
-    hash.Init();
-    hash.Update(std::vector<uint8_t>(m4, m4 + sizeof(m4) / sizeof(uint8_t) - 1));
-    hash.Final(digest);
-
-    //std::for_each(digest.begin(), digest.end(), [](uint8_t b) { printf("%02x", b); });
-    //std::cout << "\n";*/
+    std::vector<uint8_t> enc1 = eme.Encode(message, 50);
+    
+    std::vector<uint8_t> enc2 = emsa.Encode<SHA224>(message, 50);
+    
+    std::copy(enc1.begin(), enc1.end(), std::ostream_iterator<uint16_t>(std::cout, " "));
+    std::cout<<"\n";
+    
+    std::copy(enc2.begin(), enc2.end(), std::ostream_iterator<uint16_t>(std::cout, " "));
+    std::cout<<"\n";
 
     return 0;
-
     //::testing::InitGoogleTest(&argc, argv);
 
     // return RUN_ALL_TESTS();
