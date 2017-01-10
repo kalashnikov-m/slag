0dfb988c3c1750a637131c4f809af3cb6131511b
kalashnikov maxim <maxim_1981@inbox.ru>
2017-01-10 20:38:03 +0300

    pss...

--- cryptolib/src/pss.hpp
+++ cryptolib/src/pss.hpp
@@ -1,11 +1,21 @@
 #ifndef PSS_H
 #define PSS_H
 
+template<class DigestType>
 class PSS {
 
     template<class InputIterator, class OutputIterator>
     void Encode(InpitIterator first, InputIterator last, OutputIterator result) {
 	
+      size_t hLen = DigestType::size;
+      
+      DigestType digest;
+      
+      std::vector<uint8_t> hash(hLen);
+      digest(first, last, hash.begin());
+      
+      
+      
     }
 
     template<class InputIterator, class OutputIterator>
