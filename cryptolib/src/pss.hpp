8bdfa49343b036ccb5f851a3ab87aad35060058f
kalashnikov maxim <maxim_1981@inbox.ru>
2017-01-10 20:31:08 +0300

    pss.hpp (+)

---
+++ cryptolib/src/pss.hpp
@@ -0,0 +1,17 @@
+#ifndef PSS_H
+#define PSS_H
+
+class PSS {
+
+    template<class InputIterator, class OutputIterator>
+    void Encode(InpitIterator first, InputIterator last, OutputIterator result) {
+	
+    }
+
+    template<class InputIterator, class OutputIterator>
+    void Decode(InpitIterator first, InputIterator last, OutputIterator result) {
+	
+    }
+};
+
+#endif
