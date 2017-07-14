58d938b9b80615f8035fe14cbcedbafd81eac2b5
max <maxim_1981@inbox.ru>
2017-07-14 08:31:44 +0300

    rsa: enctypt, decrypt, sign

--- include/rsa/rsa.hpp
+++ include/rsa/rsa.hpp
@@ -20,6 +20,11 @@ namespace cry {
         };
 
     public:
+        void generate_key_pair(const T& p, const T& q)
+        {
+
+        }
+
         void generate_key_pair(rsa_key& pub, rsa_key& prv, const T& e, uint32_t modulusbits)
         {
             T p, q;
@@ -48,15 +53,25 @@ namespace cry {
             prv.modulus = N;
         }
 
-        void encrypt(std::vector<uint8_t>& encrypted, const std::vector<uint8_t> plain, const rsa_key& public_key)
+        void encrypt(T& encrypted, const T& plain, const rsa_key& public_key)
         {
-
+            encrypted = cry::PowMod(plain, public_key.exponent, public_key.modulus);
         }
 
-        void decrypt(std::vector<uint8_t>& plain, const std::vector<uint8_t> encrypted, const rsa_key& private_key)
+        void decrypt(T& plain, const T& encrypted, const rsa_key& private_key)
+        {
+            plain = cry::PowMod(encrypted, private_key.exponent, private_key.modulus);
+        }
+
+        void sign(T& signature, const T& plain, const rsa_key& private_key)
+        {
+            signature = cry::PowMod(plain, private_key.exponent, private_key.modulus);
+        }
+
+        /*void verify(std::vector<uint8_t>& plain, const std::vector<uint8_t> signature, const rsa_key& public_key)
         {
 
-        }
+        }*/
 
 
     };
