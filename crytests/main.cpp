d387ec3b873ec8bd79e45de99e6cbcb4e77ec800
mKalashnikov <M.Kalashnikov@aladdin-rd.ru>
2017-08-08 11:45:32 +0300

    fix

--- crytests/main.cpp
+++ crytests/main.cpp
@@ -8,50 +8,16 @@
 //#include "gtest/gtest.h"
 
 #include <algorithm.hpp>
-#include <sha1.hpp>
-#include <oid.hpp>
-#include <sha512.hpp>
-#include <sha384.hpp>
-#include <sha224.hpp>
-#include <sha256.hpp>
-#include <pkcs1v1_5.hpp>
-#include <pss.hpp>
-#include <oaep.hpp>
-#include <mgf.hpp>
 
-#include "HugeCoreTest.cpp"
-#include "HugeTest.cpp"
-
-#include <Huge.hpp>
-
-#include <vector>
-#include <random>
-
-#include <locale>
 #include <cctype>
-
-#include <algorithm>
-#include <functional>
-#include <rsa/rsa.hpp>
 
 #include "googletest/include/gtest/gtest.h"
 
 using namespace std;
 using namespace cry;
 
-int main(int argc, char** argv) {
-
-/*        cry::bigint8_t p, q;
-
-        p = cry::generate_probably_prime<cry::bigint8_t>(512, 17);
-        q = cry::generate_probably_prime<cry::bigint8_t>(512, 17);
-
-        std::cout<<"p: "<<p<<std::endl;
-        std::cout<<"q: "<<q<<std::endl;
-    }*/
-
-    //return 0;
-
+int main(int argc, char** argv)
+{
     ::testing::InitGoogleTest(&argc, argv);
 
     return RUN_ALL_TESTS();
