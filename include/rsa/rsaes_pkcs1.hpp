e56ee819d5f99fd9a8951f9b9a0278a7916331a0
mKalashnikov <M.Kalashnikov@aladdin-rd.ru>
2017-09-06 18:12:49 +0300

    добавил функции шифрования/расшифрования для схемы rsaes_pkcs1

--- include/rsa/rsaes_pkcs1.hpp
+++ include/rsa/rsaes_pkcs1.hpp
@@ -5,24 +5,41 @@
 #ifndef RSAES_PKCS1_H
 #define RSAES_PKCS1_H
 
+#include "algorithm.hpp"
 #include "basic_int.hpp"
+#include "eme_pkcs1.hpp"
 
 namespace cry
 {
 
-    template <class Encoder, class IntType = bigint8_t>
+    template <class Encoder = eme_pkcs1, class IntType = bigint8_t>
     struct rsaes_pkcs1
     {
         template <class InputIterator, class OutputIterator>
         static OutputIterator encrypt(InputIterator first, InputIterator last, OutputIterator result, const IntType& e, const IntType& n, size_t modBits)
         {
+            size_t k = modBits / 8;
+            if (modBits % 8 != 0)
+                k++;
 
-            /*auto encoded = Encoder::encode(first, last, modulusBits / 8);
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // 1. Apply the EME-PKCS1-v1_5 encoding operation to the message M to produce an encoded message EM of length k–1 octets:
+            std::vector<uint8_t> EM(k);
+            Encoder::encode(first, last, EM.begin(), k - 1);
 
-            IntType arg(encoded.begin(), encoded.end());
-            IntType result = cry::pow_mod(arg, privateExponent, modulus);
+            /////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // 2. Convert the encoded message EM to an integer message representative m
+            IntType m(EM);
 
-            signature = (std::vector<uint8_t>)result;*/
+            //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // 3. Apply the RSAEP encryption primitive to the public key(n, e) and the message representative m to produce an integer ciphertext representative c:
+            IntType c = cry::pow_mod(m, e, n);
+
+            ///////////////////////////////////////////////////////////////////////////////////
+            // 4. Convert the ciphertext representative c to a ciphertext C of length k octets
+            std::vector<uint8_t> C(c);
+
+            result = std::copy(C.begin(), C.end(), result);
 
             return result;
         }
@@ -30,6 +47,31 @@ namespace cry
         template <class InputIterator, class OutputIterator>
         static OutputIterator decrypt(InputIterator first, InputIterator last, OutputIterator result, const IntType& d, const IntType& n, size_t modBits)
         {
+            size_t k = modBits / 8;
+            if (modBits % 8 != 0)
+                k++;
+
+            ////////////////////////////////////////////////////////////////////////////////////////////
+            // 1. If the length of the ciphertext C is not k octets, output “decryption error” and stop.
+            auto cLen = std::distance(first, last);
+            if (cLen != k)
+            {
+                throw std::runtime_error("decryption error");
+            }
+
+            /////////////////////////////////////////////////////////////////////////
+            // 2. Convert the ciphertext C to an integer ciphertext representative c:
+            IntType c(first, last);
+
+            ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+            // 3. Apply the RSADP decryption primitive to the private key(n, d) and the ciphertext representative c to produce an integer message representative m:
+            IntType m = cry::pow_mod(c, d, n);
+
+            /////////////////////////////////////////////////////////////////////////////////////////
+            // 4. Convert the message representative m to an encoded message EM of length k–1 octets:
+            std::vector<uint8_t> EM(m);
+
+            result = Encoder::decode(EM.begin(), EM.end(), result);
 
             return result;
         }
